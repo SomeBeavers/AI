f2c3cbe648f7a616e19d9eb29f0f1d8d4c681beb
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-05-30 21:54:11 +0200

    Add code to class

--- Core_Lib_net7/Class1.cs
+++ Core_Lib_net7/Class1.cs
@@ -1,7 +1,23 @@
 ﻿namespace Core_Lib_net7
 {
-    public class Class1
+    /// <summary>
+    /// Represents a record struct with a Name property and an Age field.
+    /// </summary>
+    public record struct Class1(int Age)
     {
+        /// <summary>
+        /// Gets the name of this <see cref="Class1"/> instance.
+        /// </summary>
+        public string Name { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Class1"/> struct.
+        /// </summary>
+        /// <param name="name">The name of the person represented by this instance.</param>
+        /// <param name="age">The Age of the person represented by this instance.</param>
+        public Class1(string name, int age) : this(age)
+        {
+            Name = name;
+        }
     }
 }
