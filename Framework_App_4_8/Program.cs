23ce6a7ccf3647058f81290a4e0769bfdb990830
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-05-31 11:46:59 +0200

    Add projects.

---
+++ Framework_App_4_8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Framework_App_4_8
+{
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main method that is executed when the application starts.
+        /// </summary>
+        /// <param name="args">An array of command-line arguments.</param>
+        static void Main(string[] args)
+        {
+            int test = Int32.MaxValue;
+
+            Console.WriteLine("another");
+        }
+    }
+
+}
