7127f4721ba15165e02e13171e5512a2f11603de
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-06-01 14:09:01 +0200

    Add preprocessor directive.

--- Multi/Multi1.cs
+++ Multi/Multi1.cs
@@ -3,6 +3,20 @@
 
     public class Multi1
     {
+        const string myRootPath = "/src/to/my/root";
 
+        public void MyMethod()
+        {
+            string myWholeFilePath = $"{myRootPath}/README.md";
+        }
+#if NETCOREAPP
+
+
+
+        public void MyMethodForNetCoreOnly()
+        {
+            // Your .NET Core specific implementation
+        }
+#endif
     }
 }
