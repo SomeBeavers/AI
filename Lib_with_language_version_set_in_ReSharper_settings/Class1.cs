94264433efd61fa0611258b82c99241df0ec55e2
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-06-01 18:02:57 +0200

    Add projects.

---
+++ Lib_with_language_version_set_in_ReSharper_settings/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Lib_with_language_version_set_in_ReSharper_settings
+{
+    public class Class1
+    {
+
+    }
+}
--- WinForms_Core/Form1.cs
+++ WinForms_Core/Form1.cs
@@ -17,5 +17,9 @@ namespace WinForms_Core
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
