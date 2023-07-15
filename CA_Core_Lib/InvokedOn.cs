a917df3cf67c8e5beb4eacad53b5695980ad1965
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-07-15 15:05:41 +0200

    Add classes to CA test.

---
+++ CA_Core_Lib/InvokedOn.cs
@@ -0,0 +1,9 @@
+﻿namespace CA_Core_Lib;
+
+public class InvokedOn
+{
+    public Type? Declaration(Type type)
+    {
+        return type.DeclaringType;
+    }
+}
--- CA_Core_Lib/Program.cs
+++ CA_Core_Lib/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using CA_Core_Lib;
+
 Console.WriteLine("Hello, World!");
+
+new InvokedOn().Run();
