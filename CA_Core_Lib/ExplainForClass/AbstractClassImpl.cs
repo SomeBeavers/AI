2c5bbbb8ee252b212cd20f12316b4ac0d044ed46
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-07-17 16:06:36 +0200

    Add abstract class implementation

--- CA_Core_Lib/ExplainForClass/AbstractClassImpl.cs
+++ CA_Core_Lib/ExplainForClass/AbstractClassImpl.cs
@@ -2,4 +2,27 @@
 
 class AbstractClassImpl : AbstractClass
 {
+    public override string GetName(string name)
+    {
+
+    }
+    public override string GetName2(string name)
+    {
+
+    }
+    public override int GetAge(int age)
+    {
+        // Implement this method
+        throw new NotImplementedException();
+    }
+    public override InnerClass GetInnerClass(InnerClass innerClass)
+    {
+        // Implement this method
+        throw new NotImplementedException();
+    }
+    public override bool GetIsTrue(bool isTrue)
+    {
+        // Implement this method
+        throw new NotImplementedException();
+    }
 }
