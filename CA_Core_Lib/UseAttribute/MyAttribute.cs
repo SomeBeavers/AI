2d0620c857a1ed800fa19fd0e2470cc3a817eb9a
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-07-21 15:12:23 +0200

    Add attributes

---
+++ CA_Core_Lib/UseAttribute/MyAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace CA_Core_Lib.UseAttribute;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class MyAttribute<T>:Attribute where T : notnull 
+{
+    public T Parameter { get; set; }
+
+    public MyAttribute(T parameter)
+    {
+        Parameter = parameter;
+    }
+}
+
+class UseAttribute
+{
+    [My<string>(nameof(GetName))]
+    public string GetName(string name)
+    {
+        if (name == null) throw new ArgumentNullException(nameof(name));
+        return name;
+    }
+    
+    [My<int>(1)]
+    public int GetName(int name)
+    {
+        return name;
+    }
+}
