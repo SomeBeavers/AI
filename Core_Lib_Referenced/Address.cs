28b11a7854cbf7ceda8f9830af1214764297ff23
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-07-21 17:21:39 +0200

    Add tests for fields

---
+++ Core_Lib_Referenced/Address.cs
@@ -0,0 +1,25 @@
+﻿namespace Core_Lib_Referenced;
+
+public class Address<T> where T: class, IAnimalInterface
+{
+    public T? Animal;
+
+    public Address(T? animal)
+    {
+        Animal = animal;
+    }
+
+    public void Print()
+    {
+        if (Animal == null) return;
+        Console.WriteLine(Animal.Address);
+    }
+}
+
+public interface IAnimalInterface
+{
+    string Name { get; set; }
+    string Address { get; set; }
+    public string GetNameMethodFromAnimal();
+    public string GetNameMethodFromAnimal<U>(U u);
+}
