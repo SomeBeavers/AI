fc2f0f0aedee2e031a08db90f9e63291cdfb1528
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-07-25 17:30:46 +0200

    UPD

--- CA_Core_Lib/Lambda/Lambda1.cs
+++ CA_Core_Lib/Lambda/Lambda1.cs
@@ -2,10 +2,15 @@
 
 public class Lambda1
 {
+    const string s = b;
+    private  string b = "VALUE";
+
     public void Test<T>(List<T> list)
     {
         var any = list.Where(x => x != null).Any(x => x == null);
     }
+
+    public string S { get; set; init; }
 
     public void Test2()
     {
