464dfca26cfae94406f23c51fa0582cd0a43dfcc
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-07-25 15:43:50 +0200

    UPD again

--- CA_Core_Lib/InvokedOn.cs
+++ CA_Core_Lib/InvokedOn.cs
@@ -6,6 +6,7 @@ public class InvokedOn
 
     public string Declaration(List<Type> types, string? message = "This is not the null string")
     {
+        int t = 1;
         if (message is not null)
         {
             Console.WriteLine(message);
@@ -15,7 +16,20 @@ public class InvokedOn
         return types.Count > 0 ? list[0].Name : "empty list";
     }
 
-    public void Test()
+    public async Task Test()
     {
+        await using IAsyncDisposable a = null, b = null;
+        await Task.Delay(10);
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+
+    public class A
+    {
+        private string s;
+
+        void PrintS()
+        {
+        }
     }
 }
